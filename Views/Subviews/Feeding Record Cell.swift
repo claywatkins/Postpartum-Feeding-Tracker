953c71ac79f990afbd27ea4b2467d55cc329dec0
Clayton Watkins <watkinsclayton17@yahoo.com>
2023-10-19 09:32:04 -0700

    added content for feeding record cell

--- Views/Subviews/Feeding Record Cell.swift
+++ Views/Subviews/Feeding Record Cell.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct Feeding_Record_Cell: View {
+    
+    var feedingRecord: FeedingRecord
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .firstTextBaseline) {
+            Image(systemName: feedingRecord.wasBottleFed ? "waterbottle.fill" : "heart.fill")
+            if feedingRecord.wasBottleFed {
+                if let oz = feedingRecord.oz {
+                    Text("Ammount Fed: \(oz)")
+                }
+                Text("Bottle Fed")
+            } else {
+                Text("Breast Fed")
+                VStack(alignment: .leading) {
+                    if let left = feedingRecord.timeLeft {
+                        Text("Left: \(feedingRecord.date...left)")
+                            .lineLimit(0)
+                            .minimumScaleFactor(0.6)
+                    }
+                    if let right = feedingRecord.timeRight {
+                        Text("Right: \(feedingRecord.date...right)")
+                            .lineLimit(0)
+                            .minimumScaleFactor(0.6)
+                    }
+                }
+            }
+            Spacer()
+            Text("Time Nursed: \(feedingRecord.date...feedingRecord.timeNursed)")
+        }
+        .padding(.horizontal)
     }
 }
 
 #Preview {
-    Feeding_Record_Cell()
+    Feeding_Record_Cell(feedingRecord: FeedingRecord(date: .now, timeNursed: .now, side: "Left" + " " + "Right", timeLeft: .now, timeRight: .now, wasBottleFed: false, wasNursed: true))
 }
