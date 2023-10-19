20d0dc7524fc5eef8195a08ed3985acbf81f48c0
Clayton Watkins <watkinsclayton17@yahoo.com>
2023-10-19 10:12:41 -0700

    added files to project

---
+++ Extensions/TimeInterval + Ext.swift
@@ -0,0 +1,8 @@
+//
+//  TimeInterval + Ext.swift
+//  Postpartum Feeding Tracker
+//
+//  Created by Clayton Watkins on 10/19/23.
+//
+
+import Foundation
---
+++ Views/Subviews/FeedingTimer.swift
@@ -0,0 +1,18 @@
+//
+//  FeedingTimer.swift
+//  Postpartum Feeding Tracker
+//
+//  Created by Clayton Watkins on 10/19/23.
+//
+
+import SwiftUI
+
+struct FeedingTimer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedingTimer()
+}
