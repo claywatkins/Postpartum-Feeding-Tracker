f4a191d70e5391d41ac3376f6235f6176fee0768
Clayton Watkins <watkinsclayton17@yahoo.com>
2023-10-19 09:52:29 -0700

    added FeedingRecord to schema

--- Postpartum_Feeding_TrackerApp.swift
+++ Postpartum_Feeding_TrackerApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct Postpartum_Feeding_TrackerApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            FeedingRecord.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
