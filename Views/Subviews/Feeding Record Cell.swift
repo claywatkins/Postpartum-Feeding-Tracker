4164c238377a3d51566639867d1bfc7802c0001f
Clayton Watkins <watkinsclayton17@yahoo.com>
2023-10-19 09:06:15 -0700

    Created file for feeding record cell

---
+++ Views/Subviews/Feeding Record Cell.swift
@@ -0,0 +1,18 @@
+//
+//  Feeding Record Cell.swift
+//  Postpartum Feeding Tracker
+//
+//  Created by Clayton Watkins on 10/18/23.
+//
+
+import SwiftUI
+
+struct Feeding_Record_Cell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Feeding_Record_Cell()
+}
