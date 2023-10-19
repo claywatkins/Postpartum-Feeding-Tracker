4d5e876c1116b94f418110e21fa2f8777787bf24
Clayton Watkins <watkinsclayton17@yahoo.com>
2023-10-18 18:38:04 -0700

    Created groups + FeedingLog

---
+++ Views/FeedingLog.swift
@@ -0,0 +1,18 @@
+//
+//  FeedingLog.swift
+//  Postpartum Feeding Tracker
+//
+//  Created by Clayton Watkins on 10/18/23.
+//
+
+import SwiftUI
+
+struct FeedingLog: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedingLog()
+}
