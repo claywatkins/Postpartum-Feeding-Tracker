3f42036d54045996421aafea1284ca6d8c5b5d94
Clayton Watkins <watkinsclayton17@yahoo.com>
2023-10-19 09:57:59 -0700

    Created the feeding timer home screen file

---
+++ Views/FeedingTimerHome.swift
@@ -0,0 +1,18 @@
+//
+//  FeedingTimerHome.swift
+//  Postpartum Feeding Tracker
+//
+//  Created by Clayton Watkins on 10/19/23.
+//
+
+import SwiftUI
+
+struct FeedingTimerHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedingTimerHome()
+}
