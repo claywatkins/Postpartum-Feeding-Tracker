6990770cbc0f3f6074436b24fd5a732c004ae575
Clayton Watkins <watkinsclayton17@yahoo.com>
2023-10-19 11:03:58 -0700

    created timer to pause and start

--- Views/Subviews/FeedingTimer.swift
+++ Views/Subviews/FeedingTimer.swift
@@ -8,18 +8,35 @@
 import SwiftUI
 
 struct FeedingTimer: View {
-    @State private var startTime = Date.now
-    @State var interval = TimeInterval()
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var isTimerRunning: Bool = false
+    @State private var progressTime = 0
+    var minutes: Int {
+        (progressTime % 3600) / 60
+    }
+    var seconds: Int {
+        progressTime % 60
+    }
+    
+    @State private var timer: Timer?
     
     var body: some View {
         ZStack {
             VStack {
-                ZStack {
-                    Text("\(interval.format(using: [.minute, .second]))")
-                        .onReceive(timer, perform: { _ in
-                            interval = Date().timeIntervalSince(startTime)
-                        })
+                HStack {
+                    StopwatchUnit(timeUnit: minutes, timeUnitText: "MIN", color: .black)
+                    Text(":")
+                        .offset(y: -10)
+                    StopwatchUnit(timeUnit: seconds, timeUnitText: "SEC", color: .black)
+                }
+                .onTapGesture {
+                    if isTimerRunning {
+                        timer?.invalidate()
+                    } else {
+                        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
+                                                    progressTime += 1
+                                                })
+                    }
+                    isTimerRunning.toggle()
                 }
             }
         }
